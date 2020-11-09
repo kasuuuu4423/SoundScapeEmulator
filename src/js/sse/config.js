25661b6a015b0f1f019e8a20732b6fec27b5fb40
kasuuuu4423 <info@shimizoo.com>
2020-11-09 18:10:07 +0900

    [update] appのルート取得

--- src/js/sse/config.js
+++ src/js/sse/config.js
@@ -1,11 +1,32 @@
+const { ipcRenderer } = require('electron');
+
 export default class Config{
     root;
+    appRoot;
     constructor(){
         let _l = location.href;
-        _l = _l.split('/');
-        _l = _l.filter(item => {
-            return item != 'index.html';
+        _l = this.remove_fileName(_l, 'index.html');
+        this.root = _l + '/js/sse';
+
+        ipcRenderer.send('status', 'ready');
+        ipcRenderer.on('root', (e, arg) => {
+            this.appRoot = this.remove_fileName(arg);
+            //let dir_root = this.remove_fileName(arg);
         });
-        this.root = _l.join('/') + '/js/sse';
+    }
+    remove_fileName(directory){
+        let dir = directory.split('/');
+        let flg_dir = false;
+        if(dir[1] == undefined){
+            dir = dir[0].split("\\");
+            flg_dir = true;
+        }
+        dir.pop();
+        if(flg_dir){
+            return dir.join('\\');
+        }
+        else{
+            return dir.join('/');
+        }
     }
 }
